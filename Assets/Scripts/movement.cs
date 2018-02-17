68495ea4b283a1af5e9d71b5232ea605e0bd7d17
Pruthvi <pruthvi@users.noreply.github.com>
2018-02-16 20:38:38 -0500

    First workable push
    Contains some rough assets and moving tank along the terrain.

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rBody;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rBody = GetComponent<Rigidbody2D>();
+        if (moveHorizontal > 0)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+            rBody.velocity = movement * speed;
+        }
+        else if (moveHorizontal < 0)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+            rBody.velocity = movement * speed;
+        }
+        else
+        {
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+            rBody.velocity = movement * speed;
+        }
+    }
+}
