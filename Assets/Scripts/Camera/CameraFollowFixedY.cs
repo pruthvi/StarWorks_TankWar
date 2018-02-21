bbd2996cd28d2c601746fb48f7cf1c89febfd63b
luonghuy24 <mrluonghuy@gmail.com>
2018-02-21 18:12:40 -0500

    moving camera folowing player position

---
+++ Assets/Scripts/Camera/CameraFollowFixedY.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowFixedY : MonoBehaviour {
+
+	public Transform playerPosition;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		this.transform.position = new Vector3 (playerPosition.position.x, this.transform.position.y, this.transform.position.z);
+	}
+}
