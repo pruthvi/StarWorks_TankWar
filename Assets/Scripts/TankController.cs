4416b24206de38c9043a7a9cb16c29be190dabb7
luonghuy24 <mrluonghuy@gmail.com>
2018-04-01 07:25:14 -0400

    added 5 tanks

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -2,33 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class PositionsConfig {
+	public Transform exhaustPointLeft;
+	public Transform exhaustPointRight;
+	public Transform shootingPointLeft;
+	public Transform shootingPointRight;
+	public Transform tankShotPositionLeft;
+	public Transform tankShotPositionRight;
+}
+
+[System.Serializable]
+public class TankConfig {
+	public float maxSpeed = 10f;
+}
+
 public class TankController : MonoBehaviour {
+
+	public PositionsConfig positionsConfig;
+	public TankConfig tankConfig;
 
 	public enum Direction {left, right };
 
-	public float maxSpeed = 10f;
 	public Direction direction = 0;
-	public Transform exhaustPointLeft;
-	public Transform exhaustPointRight;
-	public GameObject exhaustFumeFrefab;
 	public GameObject barrel;
-	public GameObject bulletFrefab;
-	public GameObject tankShotFrefab;
-	public Transform shootingPointLeft;
-	public Transform shootingPointRight;
-	public Transform TankShotPositionLeft;
-	public Transform TankShotPositionRight;
+
 	public float bulletForce = 700.0f;
 
-	//
-	public float weaponDamage = 10.0f;
-
+	private GameObject bulletFrefab;
+	private GameObject tankShotFrefab;
+	private GameObject exhaustFumeFrefab;
 	private Rigidbody2D rBody;
 	private SpriteRenderer sRend;
 	private Animator animator;
 	private GameObject exhaustFume;
-
-
 	private AudioSource tankMove;
 	private AudioSource barrelMove;
 
@@ -36,8 +43,10 @@ public class TankController : MonoBehaviour {
 		rBody = this.GetComponent<Rigidbody2D>();
 		sRend = this.GetComponent<SpriteRenderer>();
 		animator = this.GetComponent<Animator>();	
-		exhaustFume = Instantiate (exhaustFumeFrefab, exhaustPointLeft.position, exhaustPointLeft.rotation);			
-
+		exhaustFumeFrefab = Resources.Load("Prefabs/exhaustFume") as GameObject;
+		tankShotFrefab = Resources.Load ("Prefabs/TankShot") as GameObject;
+		bulletFrefab = Resources.Load ("Prefabs/weapon_16_surprise_stones") as GameObject;
+		exhaustFume = Instantiate (exhaustFumeFrefab, positionsConfig.exhaustPointLeft.position, positionsConfig.exhaustPointLeft.rotation);			
 		AudioSource[] allMyAudioSources = GetComponents<AudioSource>();
 		tankMove = allMyAudioSources[0];
 		barrelMove = allMyAudioSources[1];
@@ -48,20 +57,19 @@ public class TankController : MonoBehaviour {
 		//Moving tank with left/right keys
 		float moveHoriz = Input.GetAxis("Horizontal");	
 
-
-		rBody.velocity = new Vector2(moveHoriz * maxSpeed, rBody.velocity.y);	
+		rBody.velocity = new Vector2(moveHoriz * tankConfig.maxSpeed, rBody.velocity.y);	
 
 		animator.SetFloat("Speed", Mathf.Abs(moveHoriz));
 		exhaustFume.GetComponent<Animator>().SetFloat("Speed", Mathf.Abs(moveHoriz));
 
 		if (moveHoriz > 0) {						
 			changeDirrection (Direction.left);
-			displayExhauseFume (exhaustPointLeft);
+			displayExhauseFume (positionsConfig.exhaustPointLeft);
 			if (!tankMove.isPlaying) tankMove.Play ();
 			
 		} else if (moveHoriz < 0) {						
 			changeDirrection (Direction.right);
-			displayExhauseFume(exhaustPointRight);
+			displayExhauseFume(positionsConfig.exhaustPointRight);
 			if (!tankMove.isPlaying) tankMove.Play ();
 		} else {
 			if (tankMove.isPlaying) tankMove.Stop ();
@@ -88,18 +96,12 @@ public class TankController : MonoBehaviour {
 	}
 
 	void shoot(){		
-		Transform shootingPoint = direction == Direction.left ? shootingPointLeft : shootingPointRight;
-		Transform tankShotPoint = direction == Direction.left ? TankShotPositionLeft : TankShotPositionRight;
+		Transform shootingPoint = direction == Direction.left ? positionsConfig.shootingPointLeft : positionsConfig.shootingPointRight;
+		Transform tankShotPoint = direction == Direction.left ? positionsConfig.tankShotPositionLeft : positionsConfig.tankShotPositionRight;
 		GameObject bullet = Instantiate (bulletFrefab, shootingPoint.position, shootingPoint.rotation);		
 		GameObject tankShot = Instantiate (tankShotFrefab, tankShotPoint.position, tankShotPoint.rotation);		
 		Destroy (tankShot, 0.4f);
 		bullet.GetComponent<Rigidbody2D> ().AddForce (bullet.transform.right * bulletForce);
-
-//		bullet.GetComponent<Rigidbody2D> ().velocity += bullet.transform.forward * 500;
-
-//		rigidbody.velocity += transform.forward * MovSpeed;
-				
-//		bullet.transform.Translate(Vector2.right * bulletForce * Time.deltaTime);
 		bullet.GetComponent<Rigidbody2D> ().velocity = Vector2.right * 10 * Time.deltaTime;
 	}
 
