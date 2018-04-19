16859c3db39bdb1bad27573b76dcd1f9b25d541e
Pruthvi <pruthvi@users.noreply.github.com>
2018-04-18 23:34:54 -0400

    Added Health Collectible
    Included Health Box in prefabs too along with script.

---
+++ Assets/GetHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetHealth : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            collision.gameObject.GetComponent<TankController>().currentHealth += 50;
+            Destroy(this.gameObject);
+            Debug.Log("Health Added");
+
+
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            collision.gameObject.GetComponent<TankController>().currentHealth += 50;
+            Destroy(this.gameObject);
+            Debug.Log("Health Added");
+
+            //				col2D.GetComponent<TankController> ().Damage (damageAmt);
+
+        }
+    }
+
+}
